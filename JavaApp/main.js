8d0b48fbcac824d27008ef0ce090afa6ef18e7d7
ziwenlin <zw.lin1998@hotmail.com>
2020-01-17 13:29:47 +0100

    New application project
    Webpage integrated in JavaFX

---
+++ JavaApp/main.js
@@ -0,0 +1,42 @@
+nav = document.getElementsByTagName('nav')[0];
+menu = document.getElementsByTagName('menu')[0];
+main = document.getElementsByTagName('main')[0];
+nav.onclick = function () {
+    nav.classList.add('hover')
+};
+menu.onclick = function () {
+    menu.classList.add('hover')
+};
+main.onclick = function () {
+    nav.classList.remove('hover')
+    menu.classList.remove('hover')
+};
+nav_items = nav.getElementsByClassName('nav_item');
+for (let i = 0; i < nav_items.length; i++) {
+    const item = nav_items[i];
+    const name = item.innerHTML.replace(/\s/g, '');
+    item.onclick = function () {
+        menu.classList.add('hover');
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            console.log(this.readyState, this.status)
+            if ((this.readyState === 4) && (this.status === 200 || this.status ===0)) {
+                menu.innerHTML = this.responseText;
+                load_select_menu();
+                load_date_picker()
+            } else if (this.status === 404) {
+                menu.innerHTML = 'Error ' + this.status;
+            }
+        };
+        xhttp.open("GET", 'menu/' + name + ".html", true);
+        xhttp.send();
+    }
+}
+
+
+
+
+
+
+
+
