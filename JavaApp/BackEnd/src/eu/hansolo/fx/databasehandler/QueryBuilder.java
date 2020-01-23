d3bdb1e611a52aa1830daea04ad995f4be9acdc5
Collin <B.C.van.Dijken@st.hanze.nl>
2020-01-23 13:13:17 +0100

    QueryBuilder changes for job

--- JavaApp/BackEnd/src/eu/hansolo/fx/databasehandler/QueryBuilder.java
+++ JavaApp/BackEnd/src/eu/hansolo/fx/databasehandler/QueryBuilder.java
@@ -38,6 +38,7 @@ public class QueryBuilder {
             query = String.format("%s ( %s <= positions.datetime <= %s )", query, date_from, date_till);
         }
 
+        // Iterates over every device in devices and adds them to the where clause
         String devicesClause = null;
         for(String device : devices){
             if (devicesClause == null){
@@ -52,4 +53,42 @@ public class QueryBuilder {
 
         return query;
     }
+
+    private String buildDateQuery(String date_till, String date_from){
+        return String.format("%s ( %s <= positions.datetime <= %s )", query, date_from, date_till);
+    }
+
+    private String buildDateClause(String time, long time_amount){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        String date_till = dtf.format(now);
+        String date_from = null;
+
+        if (time.equals("minutes")){
+            date_from = dtf.format(now.minusMinutes(time_amount));
+        }
+        else if(time.equals("hours")){
+            date_from = dtf.format(now.minusHours(time_amount));
+        }
+        else if (time.equals("days")){
+            date_from = dtf.format(now.minusDays(time_amount));
+        }
+        else if (time.equals("weeks")){
+            date_from = dtf.format(now.minusWeeks(time_amount));
+        }
+        else if (time.equals("months")){
+            date_from = dtf.format(now.minusMonths(time_amount));
+        }
+        else{
+            date_from = dtf.format(now.minusYears(time_amount));
+        }
+        return String.format(" ( %s <= positions.datetime <= %s )", date_from, date_till);
+    }
+
+    private String buildDevicesClause(ArrayList<String> devices){
+        //still need to write this
+        return "";
+
+    }
+
 }
