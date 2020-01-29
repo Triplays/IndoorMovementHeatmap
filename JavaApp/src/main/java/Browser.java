f913ef8679ec481691d018b5c53cf193d4f6a312
Collin <B.C.van.Dijken@st.hanze.nl>
2020-01-29 14:55:56 +0100

    Changed to a gradle project
    Changed project to a gradle project for it's dependencies

---
+++ JavaApp/src/main/java/Browser.java
@@ -0,0 +1,61 @@
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import netscape.javascript.JSObject;
+
+public class Browser extends Region {
+
+    final WebView browser = new WebView();
+    final WebEngine webEngine = browser.getEngine();
+    final String url = Main.class.getResource("interface/Index.html").toExternalForm();
+
+    public Browser() {
+        //apply the styles
+        getStyleClass().add("browser");
+        // load the web page
+        webEngine.load(url);
+        //add the web view to the scene
+        getChildren().add(browser);
+
+        webEngine.getLoadWorker().stateProperty().addListener(
+                (ObservableValue<? extends Worker.State> ov, Worker.State oldState,
+                 Worker.State newState) -> {
+//                    toolBar.getChildren().remove(toggleHelpTopics);
+                    if (newState == Worker.State.SUCCEEDED) {
+                        JSObject win = (JSObject) webEngine.executeScript("window");
+                        win.setMember("app", new JavaApp());
+                    }
+                });
+
+    }
+
+    private Node createSpacer() {
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        return spacer;
+    }
+
+    @Override
+    protected void layoutChildren() {
+        double w = getWidth();
+        double h = getHeight();
+        layoutInArea(browser, 0, 0, w, h, 0, HPos.CENTER, VPos.CENTER);
+    }
+
+    @Override
+    protected double computePrefWidth(double height) {
+        return 750;
+    }
+
+    @Override
+    protected double computePrefHeight(double width) {
+        return 500;
+    }
+}
---
+++ JavaApp/src/main/java/DBHandler.java
@@ -0,0 +1,94 @@
+import java.sql.*;
+import java.util.HashMap;
+
+public class DBHandler {
+    private String url;
+    private String username;
+    private String password;
+    private Connection con;
+    public Statement stmt;
+
+    public DBHandler(){
+         this( "jdbc:mysql://145.53.29.222:3306/indoormovementheatmap", "collin", "henkdetank");
+    }
+
+    public DBHandler(String url, String username, String password){
+        this.url = url;
+        this.username = username;
+        this.password = password;
+        this.con = connectDb();
+
+        try {
+            this.stmt = this.con.createStatement();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Create database connection and return it
+     * @return con
+     */
+    private Connection connectDb(){
+        try{
+            con = DriverManager.getConnection(url, username, password);
+            return con;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void dbTest() {
+        try {
+            ResultSet rs = this.stmt.executeQuery("SELECT * FROM device_types");
+            while (rs.next())
+                System.out.println(rs.getInt(1) + " " + rs.getString(2)); //should still change this aswell. dk what values it will return
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Gets data from database using the given query and returns resultset
+     * @param query This can be any kind of query that fits the database that you are connected to
+     */
+    public ResultSet getData(String query){
+        try{
+            return this.stmt.executeQuery(query);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    /**
+     * Returns hashmap of all registered types
+     * @return typeMap  Key = type_id and Value = description
+     * @throws SQLException
+     */
+    public HashMap<Integer, String> getAllTypes() throws SQLException {
+        HashMap<Integer, String> typeMap = new HashMap<>();
+        ResultSet types = this.stmt.executeQuery("SELECT * FROM device_types");
+        while(types.next()){
+            typeMap.put(types.getInt(1), types.getString(2));
+        }
+        return typeMap;
+    }
+
+    /**
+     * Returns hashmap of all registered devices
+     * @return devicesMap Key = device_id and Value = device_type
+     * @throws SQLException
+     */
+    public HashMap<String, Integer> getAllDevices() throws SQLException{
+        HashMap<String, Integer> devicesMap = new HashMap<>();
+        ResultSet devices = this.stmt.executeQuery("SELECT * FROM devices");
+        while(devices.next()){
+            devicesMap.put(devices.getString(1), devices.getInt(2));
+        }
+        return devicesMap;
+    }
+}
